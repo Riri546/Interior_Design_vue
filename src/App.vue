1c131e2c4ce6401375b21ca793e4160eee868e20
Лилия <l_loshakova123@mail.ru>
2023-08-30 20:59:02 +0300

    New file

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HomePage />
+    <BlogPage />
+    <BlogDitailsPage />
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+import BlogPage from './components/BlogPage.vue'
+import BlogDitailsPage from './components/BlogDitailsPage.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    BlogPage,
+    BlogDitailsPage
+  }
+}
+</script>
+
+<style>
+/* #app {
+} */
+</style>
